c4cce3bff17d41becf22bc492e65615ea4329e0e
Héctor André <andretorresjava@gmail.com>
2026-09-25 12:55:35 -0500

    Devtalles: Section 7

--- 1-devtalles/2-tanstack-query-issues-app/src/issues/hooks/useIssues.ts
+++ 1-devtalles/2-tanstack-query-issues-app/src/issues/hooks/useIssues.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import { getIssues } from '../actions';
@@ -9,11 +10,42 @@ interface UseIssuesProps {
 }
 
 export const useIssues = ({ state, selectedLabels }: UseIssuesProps) => {
+  const [page, setPage] = useState(1);
+
   const issuesQuery = useQuery({
-    queryKey: ['issues', { state, selectedLabels }],
-    queryFn: () => getIssues(state, selectedLabels),
+    queryKey: ['issues', { state, selectedLabels, page }],
+    queryFn: () => getIssues(state, selectedLabels, page),
     staleTime: 1000 * 60,
   });
 
-  return { issuesQuery };
+  useEffect(() => {
+    setPage(1);
+  }, [state]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [selectedLabels]);
+
+  const nextPage = () => {
+    if (issuesQuery.data?.length === 0) return;
+
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page === 1) return;
+
+    setPage((prevPage) => prevPage - 1);
+  };
+
+  return {
+    issuesQuery,
+
+    //Getters
+    page,
+
+    // Actions
+    nextPage,
+    prevPage,
+  };
 };
